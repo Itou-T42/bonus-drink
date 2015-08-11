38e27cffeefc145ba227d0a9b53f507a1b40dcfd
Itou-T42 <Itou-T42@mail.dnp.co.jp>
2015-08-11 22:24:36 +0900

    bonus_drink実装、RSpec実装、テストケース、100本購入時の本数をREADME.mdに記載

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount.to_i
+    shou = total / 3
+    amari = total % 3
+    total += shou
+      while shou >= 1 do
+        num = shou + amari
+        shou = num / 3
+        amari = num % 3
+        total += shou
+      end
+    return total
   end
 end
